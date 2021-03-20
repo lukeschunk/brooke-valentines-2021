b680ade5b3e32fe4f20cfbe4240bd0f366119091
lukeschunk <lukeschunk@gmail.com>
2021-03-20 14:39:41 -0600

    hearts

--- src/App.js
+++ src/App.js
@@ -48,7 +48,8 @@ const images = [
   },
   {
     img: smith_morehouse,
-    text: 'We had so many fun summer activities like this one. You are my #1 adventure partner.'
+    text:
+      'We had so many fun summer activities like this one. You are my #1 adventure partner.',
   },
   {
     img: barton_springs_2,
@@ -161,7 +162,45 @@ const images = [
 
 function App() {
   const [imageToShowDetails, setImageToShowDetails] = useState('');
-
+  useEffect(() => {
+    (() => {
+      let h = 70;
+      let d = n => Math.floor(Math.random() * n);
+      let k = c => {
+        let s = document.createElement('style');
+        if (!!(window.attachEvent && !window.opera)) {
+          s.styleSheet.cssText = c;
+        } else {
+          s.appendChild(document.createTextNode(c));
+        }
+        document.getElementsByTagName('head')[0].appendChild(s);
+      };
+      k(
+        '@keyframes u{0%{transform:rotate(0deg);}25%{transform:rotate(10deg);}50%{transform:rotate(0deg);}75%{transform:rotate(-10deg);}100%{transform:rotate(0deg);}};',
+      );
+      k(
+        '@keyframes m{0%{margin-top:2vh;opacity:0;}20%{opacity:1.0;margin-top:0vh;margin-left:0vw;transform:rotate(' +
+          d(360) +
+          'deg);}100%{opacity:0.2;margin-top:100vh;margin-left:' +
+          d(4) +
+          'vw;transform:rotate(1080deg);}};',
+      );
+      let w = document.createElement('div');
+      w.id = 'daWorld';
+      w.style =
+        'animation:u 60s ease-in infinite;position:fixed;top:0;right:0;bottom:0;left:0;pointer-events:none;';
+      document.body.appendChild(w);
+      while (h--) {
+        let o = document.createElement('div');
+        o.style = `pointer-events:none;opacity:0;animation:m ${d(14) +
+          6}s ease-in ${d(4000)}ms infinite;z-index:1000;position:fixed;top:${d(
+          40,
+        )}vh;left:${d(100)}vw;font-size:${d(50) + 10}px;`;
+        o.innerHTML = ['❤️', '💚', '💛', '💙', '💜'][d(5)];
+        w.appendChild(o);
+      }
+    })();
+  }, []);
   return (
     <div className="App">
       <div className="header">
@@ -198,5 +237,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
